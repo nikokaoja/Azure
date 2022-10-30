5929b709db058620a13e7ccf792b326a7a24a655
Nikola Vasiljevic <35523348+nikokaoja@users.noreply.github.com>
2022-10-30 23:21:55 +0100

    Update configure-nginx.sh

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Update apt cache.
-sudo apt-get update
+# sudo apt-get update
 
 # Install Nginx.
-sudo apt-get install -y nginx
+# sudo apt-get install -y nginx
+
+# Make directory
+sudo mkdir -p /var/www/html
+
+# Make empty HTML doc
+sudo touch /var/www/html/index.html
 
 # Set the home page.
-# echo "<html><body><h2>Welcome to Azure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
+echo "<html><body><h2>Welcome to Azure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
